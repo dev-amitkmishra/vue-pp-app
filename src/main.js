2733a9aeb55976a17fcb8c01414b24a8d226e313
Amit Mishra <dev.amitkmishra@gmail.com>
2019-01-22 17:55:52 +0530

    grid implementation

--- src/main.js
+++ src/main.js
@@ -5,4 +5,14 @@ Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  data: {
+    searchQuery: '',
+    gridColumns: ['name', 'power'],
+    gridData: [
+      { name: 'Chuck Norris', power: Infinity },
+      { name: 'Bruce Lee', power: 9000 },
+      { name: 'Jackie Chan', power: 7000 },
+      { name: 'Jet Li', power: 8000 }
+    ]
+  }
 }).$mount('#app')
